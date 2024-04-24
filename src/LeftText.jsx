1ae0e6ef5fa6d12e768f9101adb15b33d9c44cef
Alvin <Alv.Barnes@gmail.com>
2024-04-24 00:17:15 -0400

    Added code for dud code randomization

--- src/LeftText.jsx
+++ src/LeftText.jsx
@@ -42,47 +42,56 @@ const LeftText = () => {
   const setUpLetters = (array, num) => {
     const newNum = [0, 1, 2, 3, 4, 5]
     const dudLetters = [0, 1, 2, 3, 4]
+    const blockHolder = ['[]', '()', '{}', '<>']
+    const characterHolder = '!@#$%^&*_-=+|,;./'
+    const terminalWords = [[], [], [], [], [], [], [], [], []]
     const newString = []
-    const terminalWords = [`[${sevenWords[Math.floor(Math.random() * sevenWords.length)]}]`, `{${sevenWords[Math.floor(Math.random() * sevenWords.length)]}}`]
+    
     
     for (let i = 0; i < num; i++) {
-    //   if (numbers.length !== 0 && newString.length > 2) {
-    //   console.log('Num', numbers.length, newString, newString.length)
-    //   newString += sevenWords[Math.floor(Math.random() * sevenWords.length)]
-    //   setNumbers(prevState => prevState.filter((_, i) => i !== 0))
-    //   console.log('New', newString)
-      
-    // } else newString += array[Math.floor(Math.random() * array.length)]
-    
-      // if (numbers.length !== 0 && string.length > 2) {
-      //   setNumbers(prevState => prevState.filter((_, i) => i !== 0))
-      //   console.log(numbers)
-      // } else setString(prev => prev += array[Math.floor(Math.random() * array.length)])
-
-      // newString += array[Math.floor(Math.random() * array.length)]
-      
       newString.push(array[Math.floor(Math.random() * array.length)])
       
       
       console.log('FIND STRING', newString)
-     
     }
     const setUpWords = (string, number) => {
+
       console.log('FUNC', string)
       let rice = ['NOW', 'HI', 'BASTARD', 'YO', 'YES', 'BUGS']
-      
-      while (number.length !== 0 || dudLetters.length !== 0) {
+      let firstNum = 1
+      let secondNum = 12
+      while (number.length !== 0 ) {
         console.log('First line', string.indexOf(string[Math.floor(Math.random() * string.length)]), string[Math.floor(Math.random() * string.length)], number, terminalWords)
         // string.splice(string.indexOf(characters[Math.floor(Math.random() * characters.length)]), 7, sevenWords[Math.floor(Math.random() * sevenWords.length)].split(''))
-        string.splice(Math.floor(Math.random() * 204), 0, sevenWords[Math.floor(Math.random() * sevenWords.length)].split(''))
-        for (let i = 0; i < 6; i++) {
-          string.splice(Math.floor(Math.random() * 204), 0, '[' + characters[Math.floor(Math.random() * characters.length)].split('') + ']'.split('') )
-        }
+        // string.splice(Math.floor(Math.random() * num), 0, sevenWords[Math.floor(Math.random() * sevenWords.length)].split(''))
         
+        string.splice(Math.floor(Math.random() * (secondNum - firstNum + 1) + firstNum), 0, sevenWords[Math.floor(Math.random() * sevenWords.length)].split(''))
+        firstNum += 12
+        secondNum += 12
+        console.log('Numbers', firstNum, secondNum)
         number.pop()
-        dudLetters.pop()
       }
       
+      
+      let j = 0;
+      while (dudLetters.length !== 0 || j <= 8) {
+        terminalWords[j].unshift(blockHolder[Math.floor(Math.random() * blockHolder.length)][0])
+        for (let i = 0; i < 6; i++) {
+          terminalWords[j].push(characterHolder[Math.floor(Math.random() * characterHolder.length)])
+          
+        }
+        dudLetters.pop()
+        blockHolder.forEach(element => {
+          if (element[0] === terminalWords[j][0]) terminalWords[j].push(element[1])
+        })
+        j++
+      }
+      
+      for (let i = 0; i < 9; i++) {
+        string.splice(Math.floor(Math.random() * newString.length), 0, terminalWords[i])
+      }
+      
+      console.log('TERM', terminalWords, )
       console.log('RICE', rice, sevenWords[Math.floor(Math.random() * sevenWords.length)], sevenWords[Math.floor(Math.random() * sevenWords.length)].split(''))
       console.log('FLAT', string.flat())
       return string.flat()
@@ -90,7 +99,7 @@ const LeftText = () => {
     }
     return setUpWords(newString, newNum)
   }
-
+  
   
   console.log('ALL', string.length, string.indexOf(string[Math.floor(Math.random() * 204)]), numbers, words)
   // TRY RENDERING MAPPED AN ARRAY OF THE TEXT TO GET PERFECT LINE UP
@@ -130,7 +139,7 @@ const LeftText = () => {
           //   <p>{letter[1]}</p>
           // })
           
-          setUpLetters(characters, 204).map((letter, index) => {
+          setUpLetters(characters, 90).map((letter, index) => {
             return <p key={index}>{letter}</p>
           })
           // string.split('').map((letter, index) => {
