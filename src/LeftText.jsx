f6fa17458b6a82917e949348836404fa41d5fdd8
Alvin <Alv.Barnes@gmail.com>
2024-10-04 01:29:25 -0400

    Testing code

--- src/LeftText.jsx
+++ src/LeftText.jsx
@@ -1,65 +1,59 @@
 import React, { useEffect, useState } from 'react'
 import './TextStyles.css'
 
-const LeftText = ({ lettersNumbers, characters, randomLetters, setUpLetters, onMouseHover, onMouseOut, onClicked }) => {
-  const sevenWords = ['Formula','Dilemma','Diploma','Replica','Grandma','Spectra','Persona',
-  'Ammonia','Antenna','Stamina','Malaria','Militia','Vanilla','Bonanza','Inertia','Sequoia',
-  'Algebra','Cholera','Bohemia','Bologna','Alameda','Gorilla','Nirvana','Grandpa','Panacea',
-  'Alumina','Amnesia','Candida','Tequila','Regatta','Mascara','Magenta','Bulimia','Indicia',
-  'Gondola','Veranda','Urethra','Granola','Myeloma','Candela','Dyspnea','Sarcoma','Cantina',
-  'Rosacea','Regalia','Rotunda','Madrona','Corpora','Alfalfa','Myalgia','Gangsta','Bravura',
-  'Caldera','Anaemia','Tessera','Yeshiva','Trachea','Cordoba','Ganglia','Aphasia','Chimera',
-  'Rubella','Lantana','Breccia','Spatula','Hypoxia','Savanna','Paprika','Novella','Propria',
-  'Erotica','Giardia','Fuchsia','Taffeta','Cortina','Fistula','Arugula','Ricotta','Cannula',
-  'Adenoma','Beretta','Corolla','Piranha','Lasagna','Cantata','Ephedra','Mahatma','Marimba',
-  'Tempura','Naphtha','Polenta','Silesia','Viremia','Purpura','Sultana','Pergola','Emerita',
-  'Exotica','Helluva','Arabica']
+const LeftText = React.memo(({ lettersNumbers, characters, randomLetters, setUpLetters, onMouseHover, onMouseOut, onClicked }) => {
 
-  const showText = () => {
-    const rightGrid = document.getElementById('grid');
-
-    rightGrid.forEach((letter, index) => {
-      
-    })
-  }
 
   return (
-    <>
-    <div className='grid'>
-      <div className='leftgrid '>
-        <p>0x{randomLetters(lettersNumbers, 4)}</p>
-        <p>0x{randomLetters(lettersNumbers, 4)}</p>
-        <p>0x{randomLetters(lettersNumbers, 4)}</p>
-        <p>0x{randomLetters(lettersNumbers, 4)}</p>
-        <p>0x{randomLetters(lettersNumbers, 4)}</p>
-        <p>0x{randomLetters(lettersNumbers, 4)}</p>
-        <p>0x{randomLetters(lettersNumbers, 4)}</p>
-        <p>0x{randomLetters(lettersNumbers, 4)}</p>
-        <p>0x{randomLetters(lettersNumbers, 4)}</p>
-        <p>0x{randomLetters(lettersNumbers, 4)}</p>
-        <p>0x{randomLetters(lettersNumbers, 4)}</p>
-        <p>0x{randomLetters(lettersNumbers, 4)}</p>
-        <p>0x{randomLetters(lettersNumbers, 4)}</p>
-        <p>0x{randomLetters(lettersNumbers, 4)}</p>
-        <p>0x{randomLetters(lettersNumbers, 4)}</p>
-        <p>0x{randomLetters(lettersNumbers, 4)}</p>
-        <p>0x{randomLetters(lettersNumbers, 4)}</p>
-      </div>
-      
-      <div className='rightgrid ' id='grid'>
+    <main>
+      <div className='grid'>
+        <div className='leftgrid '>
+          <p>0x{randomLetters(lettersNumbers, 4)}</p>
+          <p>0x{randomLetters(lettersNumbers, 4)}</p>
+          <p>0x{randomLetters(lettersNumbers, 4)}</p>
+          <p>0x{randomLetters(lettersNumbers, 4)}</p>
+          <p>0x{randomLetters(lettersNumbers, 4)}</p>
+          <p>0x{randomLetters(lettersNumbers, 4)}</p>
+          <p>0x{randomLetters(lettersNumbers, 4)}</p>
+          <p>0x{randomLetters(lettersNumbers, 4)}</p>
+          <p>0x{randomLetters(lettersNumbers, 4)}</p>
+          <p>0x{randomLetters(lettersNumbers, 4)}</p>
+          <p>0x{randomLetters(lettersNumbers, 4)}</p>
+          <p>0x{randomLetters(lettersNumbers, 4)}</p>
+          <p>0x{randomLetters(lettersNumbers, 4)}</p>
+          <p>0x{randomLetters(lettersNumbers, 4)}</p>
+          <p>0x{randomLetters(lettersNumbers, 4)}</p>
+          <p>0x{randomLetters(lettersNumbers, 4)}</p>
+          <p>0x{randomLetters(lettersNumbers, 4)}</p>
+        </div>
         
-        {
+        <div className='rightgrid ' id='grid'>
           
-          
-          setUpLetters(characters, 90).map((letter, index) => {
-            return <p style={{cursor: 'pointer'}} className={letter} key={index} onClick={event => onClicked(event)} onMouseEnter={event => onMouseHover(event)} onMouseLeave={event => onMouseOut(event)}>{letter}</p>
-          })
-        }
-        
+          {
+            
+            
+            // setUpLetters().map((letter, index) => {
+            //   return <p style={{cursor: 'pointer'}} className={letter} key={index} onClick={event => onClicked(event)} onMouseEnter={event => onMouseHover(event)} onMouseLeave={event => onMouseOut(event)}>{letter}</p>
+            // })
+            setUpLetters()
+          }
+          <p onClick={event => onClicked(event)}>Test</p>
+        </div>
       </div>
-    </div>
-    </>
+    </main>
   )
-}
+})
+
+LeftText.displayName = 'LeftText'
+
+const Food = React.memo(( {characters, randomLetters} ) => {
+  return (
+    <div>
+
+    </div>
+  )
+})
+
+Food.displayName = 'MyFood'
 
 export default LeftText
