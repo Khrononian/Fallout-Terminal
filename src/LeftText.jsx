3835deb8d66ac400b4a10f289f9e634bde7ea26e
Alvin <Alv.Barnes@gmail.com>
2024-11-20 02:17:10 -0500

    Fixed bug

--- src/LeftText.jsx
+++ src/LeftText.jsx
@@ -16,11 +16,8 @@ const LeftText = ({ lettersNumbers, characters, randomLetters, setUpLetters, onM
         
         <div className='rightgrid ' id='grid'>
           {
-            // setUpLetters().map((letter, index) => {
-            //   return <p style={{cursor: 'pointer'}} className={letter} key={index} onClick={event => onClicked(event)} onMouseEnter={event => onMouseHover(event)} onMouseLeave={event => onMouseOut(event)}>{letter}</p>
-            // })
             setUpLetters().map((letter, index) => {
-              return <p style={{cursor: 'pointer'}} className={letter} key={index} onMouseMove={event => onMoved(event)} onClick={event => onClicked(event)} onMouseEnter={event => onMouseHover(event)} onMouseLeave={event => onMouseOut(event)}>{letter}</p>
+              return <p style={{cursor: 'pointer'}} className={letter} key={index} onClick={event => onClicked(event)} onMouseEnter={event => onMouseHover(event)} onMouseLeave={event => onMouseOut(event)}>{letter === undefined ? '!@#$%^&*()[]<>{}_-=+|,;./'.charAt(Math.floor(Math.random() * '!@#$%^&*()[]<>{}_-=+|,;./'.length)) : letter}</p>
             })
           }
         </div>
